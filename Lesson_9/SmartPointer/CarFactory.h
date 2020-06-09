9ea0424c41f62a63e453e1e3124a87512814a0e1
Ярослав Журавльов <bant1k.2xl@gmail.com>
2020-06-09 20:02:56 +0300

    divided the program into separate class files

---
+++ Lesson_9/SmartPointer/CarFactory.h
@@ -0,0 +1,8 @@
+#pragma once
+class Car{};
+
+class CarFactory
+{
+public:
+    std::unique_ptr<Car> BuildCar(const std::string& color);
+};
---
+++ Lesson_9/SmartPointer/Driver.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "Driver.h"
+#include "CarFactory.h"
+#include "Car.h"
+
+Driver::Driver(const std::string& name, std::shared_ptr<CarFactory> factory)
+    : factory_(factory)
+    , name_(name)
+{
+}
+
+void Driver::BuyCar(const std::string& color)
+{
+    car_ = factory_->BuildCar(color);
+}
+
+void Driver::Go()
+{
+    if (car_ != nullptr)
+    {
+        std::cout << name_ << " I have a car ";
+        car_->Drive();
+    }
+    else
+    {
+        std::cout << name_ << ": I'll go on foot\n";
+    }
+}
